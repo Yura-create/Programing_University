f02fce4b4b5df4dcd1a090eba6aa64deb145e375
Yura-create <67905122+Yura-create@users.noreply.github.com>
2023-07-05 13:39:36 +0300

    Add files via upload

---
+++ Individual_work/Lab3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <math.h>
+#include <windows.h>
+
+int main(){
+    //Set ukrainian language to console
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    //Main variables
+    int p, result;
+
+    //Recieve data
+    printf("Введіть розядність числа(до 30): ");
+    scanf("%d", &p);
+
+    //Filter data and calculate the result according to the input data
+    if (p > 2 && p < 30){
+        result = pow(2, p) - pow(2, p - 2);
+        printf("З розрядністю %d можна створити: %d чисел\n", p, result);
+    }
+    else if (p > 0 && p < 3){
+        result = pow(2, p);
+        printf("З розрядністю %d можна створити: %d чисел\n", p, result);
+    }
+    else{
+        printf("Ваше число не задовільняє умови(");
+    }
+
+    return 0;
+}
