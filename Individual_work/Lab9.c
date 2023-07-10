db1e59a52f7c4f4f1848ab4bed6a8ef3d3b8f825
Yura-create <67905122+Yura-create@users.noreply.github.com>
2023-07-10 18:41:14 +0300

    Add files via upload

---
+++ Individual_work/Lab9.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <windows.h>
+
+int minSteps(int x, int y){
+    //There is no need to take a single step, so return 0
+    if (x == y){
+        return 0;
+    }
+
+    int count = 1;
+    int diff = y - x;
+
+    //If the difference is less than or equal to twice the number of steps
+    //make it 0 to make the next step as long as possible
+    while (diff > count){
+        if (diff <= 2 * count){
+            diff = 0;
+        }
+        count++;
+    }
+
+    return count;
+}
+
+int main(){
+    //Set ukrainian language to console
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    //Main variables
+    int x, y;
+
+    //Receive data
+    printf("¬вед≥ть x: ");
+    scanf("%d", &x);
+    printf("¬вед≥ть y: ");
+    scanf("%d", &y);
+
+    //Calculate result and return it
+    int steps = minSteps(x, y);
+    printf("ћ≥н≥мальна к≥льк≥сть крок≥в: %d\n", steps);
+
+    return 0;
+}
